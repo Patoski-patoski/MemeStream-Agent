a15330bff11844eadeba101d27dc92f007f7524a
Okafor Patrick C <patrickpeters911@gmail.com>
2025-09-07 23:39:39 +0100

    create a start.sh script to launch both Redis and  application

---
+++ start.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+
+# Start Redis in the background
+redis-server /etc/redis/redis.conf --daemonize yes
+
+# Wait for Redis to start
+until redis-cli ping > /dev/null 2>&1; do
+  echo "Waiting for Redis to start..."
+  sleep 1
+done
+echo "Redis started."
+
+# Execute the main command as botuser
+exec su-exec botuser npm start
